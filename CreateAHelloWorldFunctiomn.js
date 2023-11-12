7754f12f452b1e24c89a9b8ccebd32560b88567c
arafat <arfatakbar090@gmail.com>
2023-11-12 22:27:04 +0530

    updates

--- CreateAHelloWorldFunctiomn.js
+++ CreateAHelloWorldFunctiomn.js
@@ -1,6 +1,8 @@
 var createHelloWorld = function() {
-    
     return function() {
         return "Hello World";
     }
 };
+
+// Calling createHelloWorld to get the inner function and then calling it
+console.log(createHelloWorld()()); // Output: Hello World
