173bf822b04b572e9a7cc9766cd825c9027c624c
arafat <arfatakbar090@gmail.com>
2023-12-01 14:16:30 +0530

    updates

--- 2.js
+++
@@ -1,8 +0,0 @@
-let sum = 0;
-// let n = prompt("Enter the value of n")
-let n = 4
-n = Number.parseInt(n)
-for(i=0; i<n; i++) {
-    sum += (i+1)
-}
-console.log("Sum of first " + n + " numbers is equals to "+ sum)
---
+++ index.js
@@ -0,0 +1,9 @@
+function OnePlusAvg (x,y){
+    return 1+(x+y)/2
+    console.log("Done")
+}
+
+let a = 7
+let b = 7
+
+console.log ("Aversge is equal to", OnePlusAvg(a,b))
