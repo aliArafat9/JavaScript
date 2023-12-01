8a8dd070ced757a9ca86a4277144a4dce6e1e444
arafat <arfatakbar090@gmail.com>
2023-12-01 16:29:58 +0530

    updates

---
+++ Fuctions.js
@@ -0,0 +1,24 @@
+//----------------------------
+function OnePlusAvg (x,y){
+    console.log("Done")
+    return 1+(x+y)/2
+}
+
+let a = 7
+let b = 7
+
+console.log ("Aversge is equal to", OnePlusAvg(a,b))
+
+//----------------------------
+const hello = () => {
+    console.log("Hello World")
+}
+
+hello()
+
+//----------------------------
+const sum = (p,q) => {
+    return p+q
+}
+
+console.log (sum(3,4))
--- index.js
+++
@@ -1,9 +0,0 @@
-function OnePlusAvg (x,y){
-    return 1+(x+y)/2
-    console.log("Done")
-}
-
-let a = 7
-let b = 7
-
-console.log ("Aversge is equal to", OnePlusAvg(a,b))
